b6e778858de155037d77d2f96a430bb96e97873f
ChenChai <chen.chai@outlook.com>
2019-07-01 15:54:28 -0400

    Removed unused code

--- app/src/main/java/ai/chench/assidua/MainActivity.java
+++ app/src/main/java/ai/chench/assidua/MainActivity.java
@@ -3,10 +3,7 @@ package ai.chench.assidua;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.fragment.app.FragmentStatePagerAdapter;
-import androidx.lifecycle.ViewModelProviders;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
@@ -19,54 +16,9 @@ import android.widget.TextView;
 
 import java.math.BigInteger;
 import java.util.Calendar;
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-
-    private TextView balanceTextView;
-
-    private EditText expenditureCostEditText;
-    private EditText expenditureNameEditText;
-
-    private Button addExpenditureButton;
-    private Button undoExpenditureButton;
-
-    private RecyclerView recyclerView;
-    private ExpenditureAdapter adapter;
-
-    private float balance;
-
-    private SharedPreferences sharedPreferences;
-    private ExpenditureViewModel viewModel;
-
-    private View.OnClickListener addClickListener = (View v) -> {
-        float expenditureValue;
-        BigInteger i = new BigInteger("5");
-
-        try {
-            expenditureValue = Float.parseFloat(expenditureCostEditText.getText().toString());
-        } catch (NumberFormatException e) {
-            expenditureCostEditText.setError(getString(R.string.error_not_a_number));
-            return;
-        }
-        if (Float.isInfinite(expenditureValue) || Float.isNaN(expenditureValue)) {
-            expenditureCostEditText.setError(getString(R.string.error_number_too_big));
-            return;
-        }
-
-        String name = expenditureNameEditText.getText().toString().trim();
-
-        viewModel.addExpenditure(new Expenditure(name.equals("") ? getString(R.string.untitled_expenditure) : name,
-                expenditureValue, Calendar.getInstance().getTime()));
-
-        expenditureNameEditText.setText("");
-        expenditureCostEditText.setText("");
-    };
-
-    private View.OnClickListener undoClickListener = (View v) -> {
-        viewModel.undoLastExpenditure();
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,6 +37,5 @@ public class MainActivity extends AppCompatActivity {
                 return 1;
             }
         });
-
     }
 }
