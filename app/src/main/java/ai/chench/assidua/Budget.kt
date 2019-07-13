dc1b5ae27eb0afe876db35ea4a5d678c0985aa8e
ChenChai <chen.chai@outlook.com>
2019-07-13 14:20:04 -0400

    Changed expenditure class to Kotlin

--- app/src/main/java/ai/chench/assidua/Budget.kt
+++ app/src/main/java/ai/chench/assidua/Budget.kt
@@ -2,4 +2,4 @@ package ai.chench.assidua
 
 import java.math.BigInteger
 
-data class Budget(var name: String, var expenditures: List<BigInteger>)
+data class Budget(var name: String, var expenditures: List<Expenditure>)
---
+++ app/src/main/java/ai/chench/assidua/Expenditure.kt
@@ -0,0 +1,6 @@
+package ai.chench.assidua
+
+import java.math.BigDecimal
+import java.util.Date
+
+data class Expenditure(var name: String, var value: BigDecimal, var date: Date)
